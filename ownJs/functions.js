38b492fb67b05b1c278deffb60b12155a6f1a9e6
natiexperiencia <natiexperiencia@gmail.com>
2016-02-21 19:01:33 +0100

    estructura ok

--- ownJs/functions.js
+++ ownJs/functions.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+	/*
+	*	Animación de paginación
+	*/
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'jswing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	/*
+	*	FIN Animación de paginación
+	*/
 	
 });
